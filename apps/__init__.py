8cdb54700eec187215a064d327c6f878777f962f
Ories Zhu <tellt@126.com>
2020-05-11 13:20:27 +0800

    初始化

---
+++ apps/__init__.py
@@ -0,0 +1,8 @@
+#_*_coding:utf-8_*_
+# Author      :ories  
+# File_Name   :__init__.py.py    
+# Create_Date :2020-05-11 13:14  
+# Description :
+# IDE         :PyCharm
+
+
---
+++ extra_apps/__init__.py
@@ -0,0 +1,8 @@
+#_*_coding:utf-8_*_
+# Author      :ories  
+# File_Name   :__init__.py.py    
+# Create_Date :2020-05-11 13:15  
+# Description :
+# IDE         :PyCharm
+
+
