7e6254c9efd06f50d96f19e62099a369686caa4c
朱骏杰 <ories.zhu@mochain.vip>
2020-05-23 22:21:43 +0800

    开始使用Xadmin替换admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from apps.users.models import UserProfile
 
@@ -7,4 +8,4 @@ class UserProfileAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserProfile, UserAdmin)
